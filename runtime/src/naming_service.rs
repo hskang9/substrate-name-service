be8139e382969cd304a1cd7e9c001bb9344945b1
hskang9 <hskang9@gmail.com>
2019-10-24 17:37:39 +0900

    fix: remove default template comment in the runtime module

--- runtime/src/naming_service.rs
+++ runtime/src/naming_service.rs
@@ -50,11 +50,6 @@ pub trait Trait: system::Trait + balances::Trait + timestamp::Trait {
 // This module's storage items.
 decl_storage! {
 	trait Store for Module<T: Trait> as NamingServiceModule {
-
-		
-		// Just a dummy storage item.
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
 		Domains get(total_domains): u64;
 		Resolver get(domain): map T::Hash => Domain<T::AccountId, T::Balance, T::Moment>;
 	}
